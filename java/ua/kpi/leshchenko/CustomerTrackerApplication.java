0d266440df26b286919bfc287e46d7f24aa69593
vitaliaventel <vitaliaventel@gmail.com>
2017-04-12 20:29:53 +0300

    Added other files

---
+++ java/ua/kpi/leshchenko/CustomerTrackerApplication.java
@@ -0,0 +1,12 @@
+package ua.kpi.leshchenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerTrackerApplication.class, args);
+	}
+}
